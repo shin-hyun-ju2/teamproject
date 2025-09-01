d69b12c36bde37b4d8dc8ed8c70f43825ff48bcb
zooH <102849251+jellyzoo@users.noreply.github.com>
2025-09-01 17:35:00 +0900

    로그인 기능 추가
    로그인 기능 추가

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import Install from './install';
+import StarRating from './components/StarRating';
+import Cart from './cart/Cart';
+import Login from './login/Login';
+
+import { Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path='/' element={
+        <>
+            <h1>PWA 테스트</h1>
+            <Install />
+            <StarRating itemId={1} />
+            <StarRating itemId={2} />
+            <StarRating itemId={3} />
+        </>
+        }></Route>
+        <Route path='/Cart' element={<Cart />} />
+        <Route path='/Login' element={<Login />} />
+
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
+
